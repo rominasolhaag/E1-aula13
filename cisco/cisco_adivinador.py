7d92465735521788d66cc89c9f9ef46ce5a0b442
SofiaxPL <106440648+SofiaxPL@users.noreply.github.com>
2022-07-05 19:33:22 -0300

    Update presentacion_German

---
+++ cisco/cisco_adivinador.py
@@ -0,0 +1,13 @@
+import os
+os.system('cls')
+palabra_secreta= 777
+
+def ingresa_numero():
+    numero=int(input("Ingrese un numero: "))
+
+
+while palabra_secreta != ingresa_numero():
+    print("Estas en mi bucle maldito muggle")
+    numero=int(input("Ingresa nuevamente un numero: "))
+    print ("saliste")
+    break
